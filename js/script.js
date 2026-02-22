9e358f980362ab337dbbb1e3f3684de1b6c6cca2
Jaime Millán <jmillangalvez@gmail.com>
2026-02-22 12:00:16 +0100

    Add new projects

--- js/script.js
+++ js/script.js
@@ -39,6 +39,8 @@ async function loadProjects() {
 		const basePath = 'projects/';
 		const projectFolders = [
 			'ElCieloDeGranadaDesdeMiBalcon',
+			'CertamenBandaVictoria',
+			'Granada1922'
 		];
 
 		for (const folder of projectFolders) {
